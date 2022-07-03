3e3bad7492847182b59cdb52892692968dae54f2
PanayotPetrov <panayot.petrov20@gmail.com>
2022-07-03 15:04:25 +0300

    Removed top level statements from worker and background services

--- PracticalTask.Services.WorkerService/Program.cs
+++ PracticalTask.Services.WorkerService/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PracticalTask.Services.WorkerService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
