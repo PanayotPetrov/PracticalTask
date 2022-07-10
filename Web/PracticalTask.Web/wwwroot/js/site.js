ccc3c3294ce0346b7f2d0f4f50994ced891f7853
PanayotPetrov <panayot.petrov20@gmail.com>
2022-07-10 22:53:25 +0300

    Moved JS to site.js

--- Web/PracticalTask.Web/wwwroot/js/site.js
+++ Web/PracticalTask.Web/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function ChangeStatus(id) {
+
+    $.ajax({
+        url: '/GuidModel/ChangeStatus',
+        type: 'POST',
+        data: JSON.stringify(id),
+        contentType: 'application/json',
+        success: function (result, status, xhr) {
+            if (xhr.status == 200) {
+                $("#" + id).remove();
+                $("#modalLabel").text('Success');
+                $("#modalBody").text('You have successfully changed the status to ready to save.');
+                $("#myModal").modal('show');
+            }
+        }
+    });
+
+    $("#btnCloseModal").click(function () {
+        $("#myModal").modal("hide");
+        $("#modalBody").text('');
+    });
+}
